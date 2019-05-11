f2e454623ae6092a84962b97bb6cb2943b1272f2
Andrey Richman <andrey.richman@gmail.com>
2019-05-12 00:18:53 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Ex02_Othelo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hola!");
+
+        }
+    }
+}
