fb02e62be37567eb8124dec0daef77b6f27e3b65
Andrey Richman <andrey.richman@gmail.com>
2019-05-12 01:12:04 +0300

    New commit

--- Program.cs
+++ Program.cs
@@ -1,19 +1,11 @@
-using System;
+﻿using System;
 namespace Ex02_Othelo
 {
     public class Program
     {
         public static void Main()
         {
-            Console.WriteLine("Hola!!!");
-<<<<<<< HEAD
-            Console.WriteLine(":(");
-=======
-            Console.WriteLine(":)");
-            Console.WriteLine(":)");
-            Console.WriteLine(":)");
->>>>>>> c5e37d001bf70cda8bfe26983ae05ed71eee170c
-
+            Console.WriteLine("Hollla!!!");
 
         }
     }
