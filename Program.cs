5ab901e16e494bda74409302821749c2632ed3b1
NivKosman <Niv.kosman95@gmail.com>
2019-05-14 18:18:47 +0300

    New message added

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace Ex02_Othelo
             Console.WriteLine("Another one");
             Console.WriteLine("Another two..");
             Console.WriteLine("#Ethalnu..\n");
+            Console.WriteLine("#Newwww123123123..\n");
 
             Move move_test;
             Console.ReadLine();
