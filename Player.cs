c432d1a7ba1ef417bdda648d99294f2487cd82b1
NivKosman <Niv.kosman95@gmail.com>
2019-05-16 14:07:43 +0300

    i created UI class and a little update in player

--- Player.cs
+++ Player.cs
@@ -8,8 +8,17 @@ namespace Ex02_Othelo
 {
      public class Player
      {
-          
+          List<Move> m_ValidMoves;
+          private int m_Score = 0;
           private bool v_IsComputer;
+          private string m_PlayerName;
+
+          public string PlayerName
+          {
+               get { return m_PlayerName; }
+               set { m_PlayerName = value; }
+          }
+
           public enum eSymbol
           {
                White,
@@ -19,11 +28,22 @@ namespace Ex02_Othelo
 
           private eSymbol m_Symbol;
 
-          public Player(ref bool i_IsComputer,ref eSymbol i_eSymbol)
+          public Player(ref bool i_IsComputer, ref eSymbol i_eSymbol)
           {
                v_IsComputer = i_IsComputer;
                m_Symbol = i_eSymbol;
+               if(v_IsComputer)
+               {
+                    m_PlayerName = "Computer";
+               }
           }
+
+          public int Score
+          {
+               get { return m_Score; }
+               set { m_Score++; }
+          }
+
 
 
      }
