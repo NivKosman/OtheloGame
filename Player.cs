dd35f5cd3f3b3679ffecec57129aa49438499fa5
NivKosman <Niv.kosman95@gmail.com>
2019-05-16 12:47:27 +0300

    i added player class

---
+++ Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02_Othelo
+{
+     public class Player
+     {
+          
+          private bool v_IsComputer;
+          public enum eSymbol
+          {
+               White,
+               Black,
+               Empty,
+          }
+
+          private eSymbol m_Symbol;
+
+          public Player(ref bool i_IsComputer,ref eSymbol i_eSymbol)
+          {
+               v_IsComputer = i_IsComputer;
+               m_Symbol = i_eSymbol;
+          }
+
+
+     }
+}
